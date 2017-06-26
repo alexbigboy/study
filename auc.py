bf9af1c1d41666d4cac04096c93ada8755fcca25
shawnxiao <shawnxiao@tencent.com>
2017-06-26 10:47:30 +0800

    1

--- auc.py
+++ auc.py
@@ -18,8 +18,8 @@ y_p = tf.placeholder(tf.float32,[SIZE])
 auc,update_op = tf.metrics.auc(labels=y, predictions=y_p)
 
 with tf.Session() as ss:
-    ss.run(tf.global_variables_initializer()) #vip
-    ss.run(tf.local_variables_initializer()) # vip
+    ss.run(tf.global_variables_initializer()) #vip,if no it,will occur error
+    ss.run(tf.local_variables_initializer()) # vip,if no it,will occur error
     print ss.run([auc,update_op], feed_dict={y: labels, y_p: pred})
     print ss.run([auc, update_op], feed_dict={y: labels1, y_p: pred})
     print ss.run([auc, update_op], feed_dict={y: labels2, y_p: pred})
